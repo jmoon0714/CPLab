a9eab3892a2bb65dd3cc548e0bc069ab28b89f5c
Kyrus Mama <kyrusmama@dhcp-mcfad-9726.eduroam.cornell.edu>
2018-09-15 10:57:00 -0400

    Updated theStar.py to have paint features

--- theStar.py
+++ theStar.py
@@ -1,38 +1,78 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 15 10:32:59 2018
-"""
 from tkinter import *
-#import tkinter as ttk
-#from ttk import *
- 
-root = Tk()
-root.title("Tk dropdown example")
- 
-# Add a grid
-mainframe = Frame(root)
-mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
-mainframe.columnconfigure(0, weight = 1)
-mainframe.rowconfigure(0, weight = 1)
-mainframe.pack(pady = 100, padx = 100)
- 
-# Create a Tkinter variable
-tkvar = StringVar(root)
- 
-# Dictionary with options
-choices = { 'Pizza','Lasagne','Fries','Fish','Potatoe'}
-tkvar.set('Pizza') # set the default option
- 
-popupMenu = OptionMenu(mainframe, tkvar, *choices)
-Label(mainframe, text="Choose a dish").grid(row = 1, column = 1)
-popupMenu.grid(row = 2, column =1)
- 
-# on change dropdown value
-def change_dropdown(*args):
-    print( tkvar.get() )
- 
-# link function to change dropdown
-tkvar.trace('w', change_dropdown)
- 
-root.mainloop()
+from tkinter.colorchooser import askcolor
+
+
+class Paint(object):
+
+    DEFAULT_PEN_SIZE = 5.0
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self):
+        self.root = Tk()
+
+        self.pen_button = Button(self.root, text='Neuron', command=self.use_pen)
+        self.pen_button.grid(row=0, column=0)
+
+        self.brush_button = Button(self.root, text='Synapse', command=self.use_brush)
+        self.brush_button.grid(row=0, column=1)
+
+        self.color_button = Button(self.root, text='color', command=self.choose_color)
+        self.color_button.grid(row=0, column=2)
+
+        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        self.eraser_button.grid(row=0, column=3)
+
+        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=4)
+
+        self.c = Canvas(self.root, bg='white', width=600, height=600)
+        self.c.grid(row=1, columnspan=5)
+
+        self.setup()
+        self.root.mainloop()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.line_width = self.choose_size_button.get()
+        self.color = self.DEFAULT_COLOR
+        self.eraser_on = False
+        self.active_button = self.pen_button
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+
+    def use_brush(self):
+        self.activate_button(self.brush_button)
+
+    def choose_color(self):
+        self.eraser_on = False
+        self.color = askcolor(color=self.color)[1]
+
+    def use_eraser(self):
+        self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button.config(relief=RAISED)
+        some_button.config(relief=SUNKEN)
+        self.active_button = some_button
+        self.eraser_on = eraser_mode
+
+    def paint(self, event):
+        self.line_width = self.choose_size_button.get()
+        paint_color = 'white' if self.eraser_on else self.color
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=paint_color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
+
+
+if __name__ == '__main__':
+    Paint()
