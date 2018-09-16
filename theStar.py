0d835961fde7ab3c3fdbefaeaa302a0c8b904f24
Kyrus Mama <kyrusmama@dhcp-rhodes-1875.eduroam.cornell.edu>
2018-09-15 21:24:27 -0400

    started theStar which can now: 	build circles for neurons 	connect neurons with synapses 	move neurons and their synapses around 	deals with errors for all the above

--- theStar.py
+++ theStar.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter.colorchooser import askcolor
-
-
+import Neuron
 class Paint(object):
 
     DEFAULT_PEN_SIZE = 5.0
@@ -10,17 +9,14 @@ class Paint(object):
     def __init__(self):
         self.root = Tk()
 
-        self.pen_button = Button(self.root, text='Neuron', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+        self.neuronButton = Button(self.root, text='Neuron', command=self.use_neuron)
+        self.neuronButton.grid(row=0, column=0)
 
-        self.brush_button = Button(self.root, text='Synapse', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
+        self.synapseButton = Button(self.root, text='Synapse', command=self.use_synapse)
+        self.synapseButton.grid(row=0, column=1)
 
         self.color_button = Button(self.root, text='color', command=self.choose_color)
         self.color_button.grid(row=0, column=2)
-
-        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
 
         self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
         self.choose_size_button.grid(row=0, column=4)
@@ -34,45 +30,214 @@ class Paint(object):
     def setup(self):
         self.old_x = None
         self.old_y = None
+        self.synapseStart = None
+        self.synapseHalfBuilt= False
+        self.tempSynapse = None
+        
         self.line_width = self.choose_size_button.get()
         self.color = self.DEFAULT_COLOR
-        self.eraser_on = False
-        self.active_button = self.pen_button
-        self.c.bind('<B1-Motion>', self.paint)
-        self.c.bind('<ButtonRelease-1>', self.reset)
+        #self.eraser_on = False
+        self.active_button = self.neuronButton
+        self.c.bind('<ButtonPress-1>', self.build)
+        self.c.bind('<B1-Motion>', self.move)
+        self.c.bind('<ButtonRelease-1>', self.drop)
+        
+        self.listOfEN=[]
+        self.listOfES=[]
 
-    def use_pen(self):
-        self.activate_button(self.pen_button)
+    def use_neuron(self):
+        self.activate_button(self.neuronButton)
+        print("Neuron currently selected")
 
-    def use_brush(self):
-        self.activate_button(self.brush_button)
+    def use_synapse(self):
+        self.activate_button(self.synapseButton)
+        print("Synapse currently selected")
+
 
     def choose_color(self):
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
 
-    def use_eraser(self):
-        self.activate_button(self.eraser_button, eraser_mode=True)
-
-    def activate_button(self, some_button, eraser_mode=False):
+    def activate_button(self, some_button):
         self.active_button.config(relief=RAISED)
         some_button.config(relief=SUNKEN)
         self.active_button = some_button
-        self.eraser_on = eraser_mode
-
-    def paint(self, event):
+        
+    def build(self, event):
+        if self.active_button==self.neuronButton:
+            self.buildNeuron(event)
+        elif self.active_button==self.synapseButton:
+            self.buildSynapse(event)
+        self.old_x=event.x
+        self.old_y=event.y
+        
+    def buildNeuron(self,event):
         self.line_width = self.choose_size_button.get()
-        paint_color = 'white' if self.eraser_on else self.color
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
-                               width=self.line_width, fill=paint_color,
-                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
-        self.old_x = event.x
-        self.old_y = event.y
+        currentColor =  self.color
+        radius=self.line_width*10
+        centerX= event.x
+        centerY= event.y
+        if self.wouldIntersect(centerX,centerY,radius)==False:
+            self.listOfEN.append(EmbeddedNeuron(self.c,centerX, centerY, radius, currentColor,0))
+            print("Neuron built")
+        else:
+            print("Error: Cannot build a neuron in this location. It is too close to another neuron.")
+            
+    def buildSynapse(self,event):
+        if self.synapseHalfBuilt==False:
+            where=self.findWhere(event.x,event.y)
+            if where!=None:
+                self.synapseStart=where
+                self.synapseHalfBuilt=True
+                self.tempSynapse=EmbeddedSynapse(self.c,self.synapseStart)
+            else:
+                print("Error: Cannot start synapse outside a neuron")
+        
+                
+    def move(self, event):
+        where=self.findWhere(self.old_x,self.old_y)
+        if self.active_button==self.neuronButton:
+            if where!=None:
+                if self.wouldIntersect(event.x,event.y,where.radius,exception=where)==False:
+                    where.redraw(event.x,event.y)
+                else:
+                    print("Error: Cannot move nueron to this location. It is too close to another neuron.")
+        
+        elif self.synapseHalfBuilt==True:
+            self.tempSynapse.drawTemp(event.x,event.y)
+        self.old_x=event.x
+        self.old_y=event.y
+
+
+        
+    def drop(self, event):
+        if self.active_button==self.synapseButton:
+            if self.synapseHalfBuilt:
+                where=self.findWhere(event.x,event.y)
+                if where!=None and where!=self.tempSynapse.NFrom:
+                    self.tempSynapse.setNToAndDraw(where)
+                    self.synapseHalfBuilt=False
+                    self.listOfES.append(self.tempSynapse)
+                    self.tempSynapse=None
+                    self.synapseStart=None
+                    print("Synapse built")
+                else:
+                    self.tempSynapse.undraw()
+                    self.synapseHalfBuilt=False
+                    self.tempSynapse=None
+                    self.synapseStart=None
+
+                
 
     def reset(self, event):
         self.old_x, self.old_y = None, None
+    
+    def findWhere(self, x,y):
+        for i in range(len(self.listOfEN)):
+            if self.listOfEN[i].checkIfInside(x,y):
+                return self.listOfEN[i]
+        return None
+    
+    def wouldIntersect(self, x,y,r,exception=None):
+        for i in range(len(self.listOfEN)):
+            if self.listOfEN[i]!=exception and self.listOfEN[i].checkIntersect(x,y,r):
+                return True
+        return False
+        
+class EmbeddedCanvas(object):
+    
+    def checkIfInside():
+        return False;
+    
+class EmbeddedNeuron(EmbeddedCanvas):
+    def __init__(self, c, centerX, centerY, radius, color, Neuron):
+        self.canvas=c
+        self.centerX=centerX
+        self.centerY=centerY
+        self.radius=radius
+        self.topLeftX=centerX-(radius/(2.0**0.5))
+        self.topLeftY=centerY-(radius/(2.0**0.5))
+        self.bottomRightX=centerX+(radius/(2.0**0.5))
+        self.bottomRightY=centerY+(radius/(2.0**0.5))
+        self.mainColor=color
+        
+        self.shape=c.create_oval(self.topLeftX,self.topLeftY,self.bottomRightX,self.bottomRightY, outline=self.mainColor, fill=self.mainColor, width=2)
+        
+        self.synapsesOut=[]
+        self.synapsesIn=[]
 
+        
+    def checkIfInside(self,x,y):
+        return (x-self.centerX)**2 + (y-self.centerY)**2 < self.radius**2
+    
+    def checkIntersect(self,x,y,r):
+        return (x-self.centerX)**2 + (y-self.centerY)**2 < (self.radius+r)**2
+    
+    def redraw(self, centerX, centerY):
+        self.undraw()
+        self.centerX=centerX
+        self.centerY=centerY
+        self.topLeftX=centerX-(self.radius/(2.0**0.5))
+        self.topLeftY=centerY-(self.radius/(2.0**0.5))
+        self.bottomRightX=centerX+(self.radius/(2.0**0.5))
+        self.bottomRightY=centerY+(self.radius/(2.0**0.5))
+        
+        self.shape=self.canvas.create_oval(self.topLeftX,self.topLeftY,\
+                                           self.bottomRightX,self.bottomRightY, outline=self.mainColor, fill=self.mainColor, width=2)
+        for S in self.synapsesOut:
+            S.redraw()
+        for S in self.synapsesIn:
+            S.redraw()
+        
+    def undraw(self):
+        self.canvas.delete(self.shape)
+        
+class EmbeddedSynapse(EmbeddedCanvas):
+    def __init__(self, c, NFrom, NTo=None, s=None):
+        self.canvas=c
+        self.NFrom=NFrom
+        self.NTo=NTo
+        self.shape=None
+        self.synapse=s
+        
+    def setNToAndDraw(self, NTo):
+        self.undraw()
+        self.NTo=NTo
+        if isinstance(self.synapse, Neuron.Synapse):
+            lineWdt= min(self.synapse.weight, self.NFrom.radius*2)
+        else:
+            lineWdt= 2
+        self.shape=self.canvas.create_line(self.NFrom.centerX, self.NFrom.centerY, self.NTo.centerX, self.NTo.centerY,\
+                                           fill=self.NFrom.mainColor, width=lineWdt)
+        if isinstance(self.NFrom, EmbeddedNeuron) and self not in self.NFrom.synapsesOut:
+            self.NFrom.synapsesOut.append(self)
+        if isinstance(self.NFrom, EmbeddedNeuron) and self not in self.NTo.synapsesIn:
+            self.NTo.synapsesIn.append(self)
+            
+    def redraw(self):
+        self.undraw()
+        if isinstance(self.synapse, Neuron.Synapse):
+            lineWdt= min(self.synapse.weight, self.NFrom.radius*2)
+        else:
+            lineWdt= 2
+        self.shape=self.canvas.create_line(self.NFrom.centerX, self.NFrom.centerY, self.NTo.centerX, self.NTo.centerY,\
+                                           fill=self.NFrom.mainColor, width=lineWdt)
+        
+    def drawTemp(self, x, y):
+        self.undraw()
+        if isinstance(self.synapse, Neuron.Synapse):
+            lineWdt= min(self.synapse.weight, self.NFrom.radius*2)
+        else:
+            lineWdt= 2
+        self.shape=self.canvas.create_line(self.NFrom.centerX, self.NFrom.centerY, x, y,\
+                                           fill=self.NFrom.mainColor, width=lineWdt)
+
+    def undraw(self):
+        if self.shape!=None:
+            self.canvas.delete(self.shape)
+            
+        
+        
 
 if __name__ == '__main__':
     Paint()
